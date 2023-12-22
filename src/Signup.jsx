4880bc10c3bdae27c862482609a001b49314992f
Ragavan0701 <naveen07nkb@gmail.com>
2023-12-22 15:23:22 +0530

    edit

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Signup = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+
+    // Clear previous errors
+    setErrors({});
+
+    // Basic validation
+    const newErrors = {};
+
+    if (!username.trim()) {
+      newErrors.username = 'Username is required.';
+    }
+
+    // Check for valid email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email.trim())) {
+      newErrors.email = 'Invalid email format.';
+    }
+
+    if (!password.trim()) {
+      newErrors.password = 'Password is required.';
+    }
+
+    // Check for minimum password length
+    if (password.length < 6) {
+      newErrors.password = 'Password must be at least 6 characters.';
+    }
+
+    // If there are errors, update the state and stop form submission
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      return;
+    }
+
+    // Perform your signup logic here
+    // For example, you can send a request to your backend to create a new user
+
+    // Show alert message after successful registration
+    alert(`Registration successful, ${username}! Now login with the registered email and password.`);
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-200">
       <div className="bg-white p-4 md:p-10 rounded-lg shadow-md w-full md:w-96">
         <h1 className="text-2xl md:text-4xl font-bold mb-4 md:mb-6 text-center">Sign Up</h1>
-        <form className="space-y-3">
+        <form className="space-y-3" onSubmit={handleSignup}>
           <div className="mb-3">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
               Username
@@ -14,8 +60,11 @@ const Signup = () => {
               type="text"
               id="username"
               name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               className="w-full p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
             />
+            <p className="text-red-500">{errors.username}</p>
           </div>
           <div className="mb-3">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
@@ -25,8 +74,11 @@ const Signup = () => {
               type="email"
               id="email"
               name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="w-full p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
             />
+            <p className="text-red-500">{errors.email}</p>
           </div>
           <div className="mb-3">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
@@ -36,8 +88,11 @@ const Signup = () => {
               type="password"
               id="password"
               name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="w-full p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
             />
+            <p className="text-red-500">{errors.password}</p>
           </div>
           <button
             type="submit"
